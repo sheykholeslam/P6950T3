c12d2282058c5303836678aca3ac5d8ec778302f
r97mlr <r97mlr@mun.ca>
2016-06-10 23:20:09 -0700

    Re Calculate Percentile

--- source/optionaltask_1.py
+++ source/optionaltask_1.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[37]:
+# In[1]:
 
 import wget
 import numpy as np
@@ -15,16 +15,29 @@ from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, DataRange1d, Range1d, VBox, HBox, Select
 from bokeh.palettes import Blues4
 
+def percentile_Calculation(MinTemp,MaxTemp,percent):
+    MinTempPercentile = []
+    MaxTempPercentile = []
+    percent = percent/100
+    for index in range(len(MinTemp)):
+        MinTempPercentile.append(MinTemp[index]+(MinTemp[index]*percent))
+        MaxTempPercentile.append(MaxTemp[index]-(MaxTemp[index]*percent))
+    return MinTempPercentile,MaxTempPercentile
+
+
 def make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25_Min,Parcentile_25_Max,MinTemp,MaxTemp,plotDate):
     plot = Figure(x_axis_type="datetime", plot_width=1000, tools="", toolbar_location=None)
     plot.title = "GDD"
     colors = Blues4[0:3]
     
-    plot.quad(top=Parcentile_5_Max, bottom=Parcentile_5_Min, left='left',right='right',source=source,color="#00ffcc", legend="Percentile 5-95")
-    plot.quad(top=Parcentile_25_Max, bottom=Parcentile_25_Min,left='left',right='right',source=source,color="#ffff00",legend="percentile 25-75")
+    plot.quad(top=Parcentile_5_Max, bottom=Parcentile_5_Min, left='left',right='right',
+              source=source,color="#e67300", legend="Percentile 5-95")
+    plot.quad(top=MinTemp, bottom=MaxTemp, left='left',right='right',
+              source=source,color="#00ffcc", legend="Actual")
+    plot.quad(top=Parcentile_25_Max, bottom=Parcentile_25_Min,left='left',right='right',
+              source=source,color="#66ccff",legend="percentile 25-75")
     plot.line(plotDate,AverageTemp,source=source,line_color='Red', line_width=0.5, legend='AverageTemp')
-    
-    # fixed attributes
+   
     plot.border_fill_color = "whitesmoke"
     plot.xaxis.axis_label = None
     plot.yaxis.axis_label = "Temperature (C)"
@@ -45,39 +58,28 @@ Data = pd.DataFrame(Hourly_Data, columns = ['Date/Time','Max Temp (Â°C)', 'Min
 Data.replace('', np.nan, inplace = True)
 Data = Data.dropna()
 Date,MinTemp, MaxTemp = np.array(Data['Date/Time']),np.array(Data['Min Temp (Â°C)']), np.array(Data['Max Temp (Â°C)'])
-
 Data['date'] = pd.to_datetime(Date)
 Data['left'] = Data.date - pd.DateOffset(days=0.5)
 Data['right'] = Data.date + pd.DateOffset(days=0.5)
 PlotDate =[]
-plotDate = Data['left']
+plotDate = Data['right']
 Data = Data.set_index(['date'])
 Data.sort_index(inplace=True)
 source = ColumnDataSource(data=Data)
-
 AverageTemp = []
 for index in range(len(MinTemp)):
     Average=(MinTemp[index]+MaxTemp[index])/2
     AverageTemp.append(Average)
+    
+percent = 5
+Min_5_95, Max_5_95 =percentile_Calculation(MinTemp,MaxTemp,percent)
+percent = 25
+Min_25_75, Max_25_75 =percentile_Calculation(MinTemp,MaxTemp,percent)
 
-Parcentile_5_Min = []
-Parcentile_5_Max = []
-Parcentile_25_Min = []
-Parcentile_25_Max = []
-FivePercent = int(365*.05)
-TwentyFivePercent = int(365*.25)
-## Delete 5 % lowest From MINTEMP and 5% Highest from MAXTEMP
-Parcentile_5_Min = np.delete(MinTemp,MinTemp.argsort()[-FivePercent:])
-Parcentile_5_Max = np.delete(MaxTemp,MaxTemp.argsort()[-FivePercent:])
-## Delete 25 % lowest From MINTEMP and 25% Highst from MAX TEMP
-Parcentile_25_Min = np.delete(MinTemp,MinTemp.argsort()[-TwentyFivePercent:])
-Parcentile_25_Max = np.delete(MaxTemp,MaxTemp.argsort()[-TwentyFivePercent:])
-
-plot = make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25_Min,Parcentile_25_Max,MinTemp,MaxTemp,plotDate)
+plot = make_plot(source,AverageTemp,Min_5_95,Max_5_95,Min_25_75,Max_25_75,
+                 MinTemp,MaxTemp,plotDate)
 output_file("GDD.html", title="GDD Example")
 show(plot)
-
-
 
 
 # In[ ]:
