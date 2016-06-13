1ebaf7a44ea4c944977fb35ec40a19fea1138f68
mmh474 <mmh474@mun.ca>
2016-06-13 01:06:52 -0230

    Updating optional task # 1

--- source/optionaltask_1.py
+++ source/optionaltask_1.py
@@ -22,13 +22,7 @@ def percentile_Calculation(MinTemp,MaxTemp,percent):
     return MinTempPercentile,MaxTempPercentile
 
 def make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25_Min,Parcentile_25_Max,MinTemp,MaxTemp,plotDate):
-<<<<<<< HEAD
     plot = Figure(title='Optional Task # 1 : Growing Degree-day', x_axis_type="datetime", plot_width=1000, title_text_font_size='12pt', tools="", toolbar_location=None)
-=======
-    
-    plot = Figure(x_axis_type="datetime", plot_width=1000, tools="", toolbar_location=None)
-    plot.title = "GDD"
->>>>>>> origin/master
     colors = Blues4[0:3]
     
     plot.circle(MaxTemp,MinTemp, alpha=0.9, color="#66ff33", fill_alpha=0.2, size=10,source=source,legend ='2015')
@@ -38,15 +32,8 @@ def make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25
               source=source,color="#00ffcc", legend="Actual")
     plot.quad(top=Parcentile_25_Max, bottom=Parcentile_25_Min,left='left',right='right',
               source=source,color="#66ccff",legend="percentile 25-75")
-<<<<<<< HEAD
     plot.line(plotDate,AverageTemp,source=source,line_color='Red', line_width=0.75, legend='AverageTemp')
-      
-=======
-    plot.line(plotDate,AverageTemp,source=source,line_color='Red', line_width=0.5, legend='AverageTemp')
-    
-    
-   
->>>>>>> origin/master
+
     plot.border_fill_color = "whitesmoke"
     plot.xaxis.axis_label = "Months"
     plot.yaxis.axis_label = "Temperature (C)"
@@ -55,7 +42,6 @@ def make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25
     plot.axis.axis_label_text_font_style = "bold"
     plot.x_range = DataRange1d(range_padding=0.0, bounds=None)
     plot.grid.grid_line_alpha = 0.3
-<<<<<<< HEAD
     plot.grid[0].ticker.desired_num_ticks = 12
     return plot
 
@@ -87,40 +73,4 @@ def Main():
 
 if __name__ == '__main__':
     Main()
-=======
-    plot.grid[0].ticker.desired_num_ticks = 20
 
-    return plot
-
-
-CurrentPath = os.getcwd()
-FilePath= (CurrentPath+'/DataFiles/GDD_Data.csv')
-Hourly_Data = pd.read_csv(FilePath, encoding = 'ISO-8859-1', delimiter = "\t" ,skiprows=0)
-Data = pd.DataFrame(Hourly_Data)
-Data.replace('', np.nan, inplace = True)
-Data = Data.dropna()
-Index = Data.keys()
-Date, MaxTemp, MinTemp = np.array(Data[Index[1]]),np.array(Data[Index[2]]), np.array(Data[Index[3]])
-Data['date'] = pd.to_datetime(Date)
-Data['left'] = Data.date - pd.DateOffset(days=0.5)
-Data['right'] = Data.date + pd.DateOffset(days=0.5)
-PlotDate =[]
-plotDate = Data['right']
-Data = Data.set_index(['date'])
-Data.sort_index(inplace=True)
-source = ColumnDataSource(data=Data)
-AverageTemp = []
-for index in range(len(MinTemp)):
-    Average=(MinTemp[index]+MaxTemp[index])/2
-    AverageTemp.append(Average)
-    
-percent = 5
-Min_5_95, Max_5_95 =percentile_Calculation(MinTemp,MaxTemp,percent)
-percent = 25
-Min_25_75, Max_25_75 =percentile_Calculation(MinTemp,MaxTemp,percent)
-
-plot = make_plot(source,AverageTemp,Min_5_95,Max_5_95,Min_25_75,Max_25_75,
-                 MinTemp,MaxTemp,plotDate)
-output_file("GDD.html", title="GDD Example")
-show(plot)
->>>>>>> origin/master
