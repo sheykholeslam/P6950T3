1edc6ac761dd26cf3644b6fa34e715bf6d449d5f
lutforrahman2015 <r97mlr@mun.ca>
2016-06-13 21:03:48 -0230

    Re-Calculate Min_Max Percentile

--- source/opTask1.py
+++ source/opTask1.py
@@ -14,35 +14,59 @@ import argparse
 from extract_data_from_csv import extract_data_from_csv
 
 def percentile_Calculation(MinTemp,MaxTemp,percent):
-    MinTempPercentile = []
-    MaxTempPercentile = []
-    percent = percent/100
-    for index in range(len(MinTemp)):
-        MinTempPercentile.append(MinTemp[index]+(MinTemp[index]*percent))
-        MaxTempPercentile.append(MaxTemp[index]-(MaxTemp[index]*percent))
-    return MinTempPercentile,MaxTempPercentile
+   
+    percentile = percent/100
+    Extreme_Min_Max = int(365*percentile)
+    
+    ## Replace with percentile of Highest Temp From MaxTEMP   
+    MaxIndexDelete = MaxTemp.argsort()[-Extreme_Min_Max:]
+    MaxIndexDelete = MaxIndexDelete[:Extreme_Min_Max]
+    for index in MaxIndexDelete:      
+        if (index == 364):
+            MaxTemp[index]=(MaxTemp[index-1]+MaxTemp[index]) /2 
+        elif (index == 0):
+            MaxTemp[index]=(MaxTemp[index+1]+MaxTemp[index])/2 
+        else:
+            MaxTemp[index]=(MaxTemp[index-1]+MaxTemp[index+1])/2 
+    ## Replace with percentile of Lowest Temp From MaxTEMP
+    MinIndexDelete = MinTemp.argsort()[:-Extreme_Min_Max]
+    MinIndex= MinIndexDelete[:Extreme_Min_Max]
+    for index in MinIndex:
+        if (index == 364):
+            MinTemp[index]=(MinTemp[index-1]+MinTemp[index])/2
+        elif (index == 0):
+            MinTemp[index]=(MinTemp[index+1]+MinTemp[index])/2
+        else:
+            MinTemp[index]=(MinTemp[index-1]+MinTemp[index+1])/2
+    
+    return MinTemp,MaxTemp
 
-def make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25_Min,Parcentile_25_Max,MinTemp,MaxTemp,plotDate,cityName):
-    plot = Figure(title='Optional Task # 1 : Growing Degree-day for '+cityName, x_axis_type="datetime", plot_width=1000, title_text_font_size='12pt', tools="", toolbar_location=None)
+
+def make_plot(source,AverageTemp,Parcentile_5_Min,Parcentile_5_Max,Parcentile_25_Min,Parcentile_25_Max,MinTemp,MaxTemp,plotDate):
+    
+    plot = Figure(x_axis_type="datetime", plot_width=1000, tools="", toolbar_location=None)
+    plot.title = "GDD"
     colors = Blues4[0:3]
     
+    
     plot.circle(MaxTemp,MinTemp, alpha=0.9, color="#66ff33", fill_alpha=0.2, size=10,source=source,legend ='2015')
-    plot.quad(top=Parcentile_5_Max, bottom=Parcentile_5_Min, left='left',right='right',
-              source=source,color="#e67300", legend="Percentile 5-95")
-    plot.quad(top=Parcentile_25_Max, bottom=Parcentile_25_Min,left='left',right='right',
-              source=source,color="#66ccff",legend="percentile 25-75")
-    plot.line(plotDate,AverageTemp,source=source,line_color='Red', line_width=0.75, legend='AverageTemp')
-
+    plot.quad(top=Parcentile_5_Max, bottom=Parcentile_5_Min, left='left',right='right',source=source,color="#000000", legend="Percentile 5-95")
+    plot.quad(top=Parcentile_25_Max, bottom=Parcentile_25_Min,left='left',right='right', source=source,color="#66ccff",legend="percentile 25-75")
+    plot.line(plotDate,AverageTemp,source=source,line_color='Red', line_width=0.5, legend='AverageTemp')
+    
+   
     plot.border_fill_color = "whitesmoke"
-    plot.xaxis.axis_label = "Months"
+    plot.xaxis.axis_label = None
     plot.yaxis.axis_label = "Temperature (C)"
-    plot.axis.major_label_text_font_size = "10pt"
-    plot.axis.axis_label_text_font_size = "12pt"
+    plot.axis.major_label_text_font_size = "8pt"
+    plot.axis.axis_label_text_font_size = "8pt"
     plot.axis.axis_label_text_font_style = "bold"
     plot.x_range = DataRange1d(range_padding=0.0, bounds=None)
     plot.grid.grid_line_alpha = 0.3
     plot.grid[0].ticker.desired_num_ticks = 12
+
     return plot
+
 
 def Main():
     parser = argparse.ArgumentParser()
@@ -61,17 +85,24 @@ def Main():
         Data = Data.set_index(['date'])
         Data.sort_index(inplace=True)
         source = ColumnDataSource(data=Data)
+        MinTempOrigin1 = []
+        MaxTempOrigin2 = []
+        for index in range(len(MinTemp)):
+	        MinTempOrigin1.append(MinTemp[index])
+	        MaxTempOrigin2.append(MaxTemp[index])
+        MinTempOrigin = np.asarray(MinTempOrigin1)
+        MaxTempOrigin = np.asarray(MaxTempOrigin2)
+
         AverageTemp = []
         for index in range(len(MinTemp)):
-            Average=(MinTemp[index]+MaxTemp[index])/2
-            AverageTemp.append(Average)
-    
-        percent = 5
-        Min_5_95, Max_5_95 =percentile_Calculation(MinTemp,MaxTemp,percent)
-        percent = 25
-        Min_25_75, Max_25_75 =percentile_Calculation(MinTemp,MaxTemp,percent)
+	        Average=(MinTemp[index]+MaxTemp[index])/2
+	        AverageTemp.append(Average)
 
-        plot = make_plot(source,AverageTemp,Min_5_95,Max_5_95,Min_25_75,Max_25_75, MinTemp,MaxTemp,plotDate,args.cityName[i])
+        percent = 5
+        Min_5_95, Max_5_95 = percentile_Calculation(MinTemp,MaxTemp,percent)
+        percent = 25
+        Min_25_75, Max_25_75 = percentile_Calculation(MinTempOrigin,MaxTempOrigin,percent)
+        plot = make_plot(source,AverageTemp,Min_5_95,Max_5_95,Min_25_75,Max_25_75,MinTemp,MaxTemp,plotDate)
         output_file("./Plots/Op1_"+args.cityName[i]+".html", title="Optional Task # 1 ("+args.cityName[i]+")")
         save(plot)
 
