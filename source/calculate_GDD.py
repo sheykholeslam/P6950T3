831219864baf5e395284e0e5c9921b2f891fed28
Mohammad Hassan <mahmud.sust@gmail.com>
2016-06-14 15:43:22 -0230

    Update calculate_GDD.py

--- source/calculate_GDD.py
+++ source/calculate_GDD.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from save_data_as_csv import save_data_as_csv
 
+# Checking GDD values. If the value < 0 than value = 0. 
 def checkGDD(values):
     GDD = []
     item = 0
@@ -12,13 +13,15 @@ def checkGDD(values):
             item += i
         GDD.append(item)
     return GDD
-	
+
+# Taking the complete DataFrame and calculating GDD. Than add the GDD values as a new column. 
 def calculate_GDD(Data, baseTemp):	
     Data['GDD'] = ((Data['Max Temp (Â°C)'] + Data['Min Temp (Â°C)'])/2)- baseTemp
     Data['GDD'] = checkGDD(Data['GDD']) 
     return Data
 	
 def Main():
+# Taking the arguments from command line. 
     parser = argparse.ArgumentParser()
     parser.add_argument("baseTemp", help="Please set base temperature.", type=int)
     parser.add_argument("-st", dest="stationId", nargs = '*', help="Please provide a list of station Id.")
@@ -27,14 +30,18 @@ def Main():
     args = parser.parse_args()
 	
     for i in range(len(args.stationId)):
+    	# Reading the data from downloaded .csv files. 
         CurrentPath = os.getcwd()
         FilePath= (CurrentPath+'/DataFiles/GDD_Data_'+args.cityName[i]+'.csv')
         Hourly_Data = pd.read_csv(FilePath, encoding = 'ISO-8859-1', delimiter = ',' ,skiprows=0)
         Data = pd.DataFrame(Hourly_Data, columns = ['Date/Time','Max Temp (Â°C)', 'Min Temp (Â°C)'])
         Data.replace('', np.nan, inplace = True)
         Data = Data.dropna()
-		
+	
+	# Calling calculate_GDD function to calculate GDD
         GDD_Data = calculate_GDD(Data, args.baseTemp)
+        
+        # Saving back the updated DataFrame to .csv. 
         save_data_as_csv(GDD_Data, FilePath)
 		
 if __name__ == '__main__':
