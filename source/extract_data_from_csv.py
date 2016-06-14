7683931540383efd33437473f3dc15687045fb61
Mohammad Hassan <mahmud.sust@gmail.com>
2016-06-14 15:47:41 -0230

    Update extract_data_from_csv.py

--- source/extract_data_from_csv.py
+++ source/extract_data_from_csv.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 
+# Read the data from a csv file by given file path.
 def extract_data_from_csv(FilePath):
     File_Data = pd.read_csv(FilePath, encoding = 'ISO-8859-1', delimiter = ',' ,skiprows=0)
     Data = pd.DataFrame(File_Data)
