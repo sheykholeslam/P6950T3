25907d9fa0de8dcdf0cde5dee176f083171f5bfd
lutforrahman2015 <r97mlr@mun.ca>
2016-06-14 23:29:47 -0230

    Re-Calculate Group

--- source/finalTask.py
+++ source/finalTask.py
@@ -7,6 +7,35 @@ import os
 import numpy as np
 import pandas as pd
 
+## Reference
+## http://store.msuextension.org/publications/AgandNaturalResources/MT200103AG.pdf
+## GDD For BARLEY,
+## The GDD of BARLEY 'Emergence':109-145,'Leaf_development':146-184,'Tillering':185-360,'Stem_elongation':489-555
+
+def getGroup(GDD):
+    group = []
+    for i in GDD:
+        if( i >= 109 and i <= 145):
+            group.append('Emergence')
+        elif ( i >= 146 and i <= 184):
+            group.append('Leaf_development')
+        elif ( i >= 185 and i <= 360):
+            group.append('Tillering')
+        elif ( i >= 361 and i <= 555):
+            group.append('Stem_elongation')
+        elif ( i >= 556 and i <= 936):
+            group.append('Anthesis')
+        elif ( i >= 937 and i <= 1145):
+            group.append('Seed_fill')
+        elif ( i >= 1146 and i <= 1438):
+            group.append('Dough_stage')
+        elif ( i >= 1439 and i <= 1522):
+            group.append('Maturity_complete')
+        else:
+            group.append('No Growth')
+
+    return group
+
 CurrentPath = os.getcwd()
 FilePath= (CurrentPath+'/DataFiles/GDD_Data.csv')
 Hourly_Data = pd.read_csv(FilePath, encoding = 'ISO-8859-1', delimiter = "\t" ,skiprows=0)
@@ -15,16 +44,12 @@ Data.replace('', np.nan, inplace = True)
 Data = Data.dropna()
 Index = Data.keys()
 Date, MinTemp, MaxTemp, GDD = np.array(Data[Index[0]]),np.array(Data[Index[1]]), np.array(Data[Index[2]]), np.array(Data[Index[5]])
-Data['group'] = Data['GDD'].map(lambda x:1 if x > 106 and x<=550 else 0)
+Data['group'] = getGroup(Data[Index[5]])
 Data['month'] = pd.DatetimeIndex(Data['date']).month
 
-## Reference
-## http://store.msuextension.org/publications/AgandNaturalResources/MT200103AG.pdf
-## GDD For BARLEY, WHEAT AND CANARY_SEED
-## The GDD of BARLEY 'Emergence':109-145,'Leaf_development':145-184,'Tillering':308-360,'Stem_elongation':489-555
 
 p = Bar(Data,label='month', values='GDD',agg='median', group='group',
-        title="GDD of Stem_elongation of Barley", legend='top_right')
+        title="GDD of Barley In Bar", legend='top_left')
 
-output_file("bar.html")
+output_file("GDDVisualization.html")
 show(p)
