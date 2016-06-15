5440208a7df24b998ac873a677c384ad1a6f474a
sheykholeslam <mhs137@mun.ca>
2016-06-15 02:41:53 -0230

    Last Op5 working with BOKEH SERVER

--- source/opTask5.py
+++ source/opTask5.py
@@ -5,13 +5,39 @@ import wget
 import os
 import argparse
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, DataRange1d, Range1d, VBox, HBox, Select
+from bokeh.models import ColumnDataSource, DataRange1d, Range1d, VBox, HBox, Select, BoxSelectTool
 from bokeh.palettes import Blues4
 from bokeh.plotting import Figure, output_file, show, save
 from scipy.signal import savgol_filter
-from extract_data_from_csv import extract_data_from_csv
 
-# Read data from .csv and manipulating the data based on program need
+source = ColumnDataSource()
+
+stationId = [50089, 51157, 50430]
+cityName = ['St_Johns', 'Montreal','Calgary']
+	
+cityData = {}
+cities = { 
+cityName[0] : {'ID':stationId[0]},
+cityName[1] : {'ID':stationId[1]},
+cityName[2] : {'ID':stationId[2]}
+}
+	
+city = cityName[0]
+city_select = Select(value=city, title='City:', options=list(cities.keys()))
+
+plot = Figure()
+
+def extract_data_from_csv(FilePath):
+    File_Data = pd.read_csv(FilePath, encoding = 'ISO-8859-1', delimiter = ',' ,skiprows=0)
+    Data = pd.DataFrame(File_Data)
+    Data.replace('', np.nan, inplace = True)
+    Data = Data.dropna()
+    Index = Data.keys()
+    Date, maxTemp, minTemp = np.array(Data[Index[1]]),np.array(Data[Index[2]]), np.array(Data[Index[3]])
+    
+    return Data, Date, maxTemp, minTemp
+
+
 def DataSet(cityName):
     CurrentPath = os.getcwd()
     FilePath= (CurrentPath+'/DataFiles/GDD_Data_'+cityName+'.csv')
@@ -35,10 +61,12 @@ def DataSet(cityName):
 
 
 def make_plot(src, city):
-    plot = Figure(x_axis_type="datetime", plot_width=1000, title_text_font_size='12pt', tools="", toolbar_location=None)
+    TOOLS = [BoxSelectTool()]
+    global plot
+    plot = Figure(x_axis_type="datetime", plot_width=1000, title_text_font_size='12pt', tools=TOOLS)
     plot.title = city
     colors = Blues4[0:3]
-    plot.line(x='date', y='GDD',source=src)
+    plot.line(x='date', y='GDD',source=src, line_width=4)
     
     plot.border_fill_color = "whitesmoke"
     plot.xaxis.axis_label = "Months"
@@ -51,47 +79,45 @@ def make_plot(src, city):
 
     return plot
 
-# set up callbacks for list select city data update interaction
+# set up callbacks
 def update_plot(attrname, old, new):
     src = DataSet(city_select.value)
     source.data.update(src.data)
+    global plot
     plot.title = city_select.value
-
+#    plot.line(x='date', y='GDD',source=source, line_width=4)
+#    plot=make_plot(src,city_select.value)
+ 
 
 def Main():
-    # Taking the arguments from command line.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-st", dest="stationId", nargs = '*', help="Please provide a list of station Id.")
-    parser.add_argument("-ct", dest="cityName", nargs = '*', help="Please provide a list of city names corresponding to stations.")
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("-st", dest="stationId", nargs = '*', help="Please provide a list of station Id.")
+#    parser.add_argument("-ct", dest="cityName", nargs = '*', help="Please provide a list of city names corresponding to stations.")
 	
-    args = parser.parse_args()
+#    args = parser.parse_args()
 	
-    cityData = {}
+#city = args.cityName[0]
 	
-	# Creating an object to make the list of dropdown and pass values to the make plot function. 
-    cities = { 
-        args.cityName[0] : {'ID':args.stationId[0]},
-        args.cityName[1] : {'ID':args.stationId[1]},
-        args.cityName[2] : {'ID':args.stationId[2]}
-    }
+    #cities = { 
+    #    args.cityName[0] : {'ID':args.stationId[0]},
+    #    args.cityName[1] : {'ID':args.stationId[1]},
+    #    args.cityName[2] : {'ID':args.stationId[2]}
+    #}
 	
-    city = args.cityName[0]
-    city_select = Select(value=city, title='City:', options=list(cities.keys()))
+   
+#    for c in cities.keys():
     
-    for c in cities.keys():
-        cityData[c] = DataSet(c)
-
-    source = cityData[city]
+    global source
+    source = DataSet('St_Johns')
     plot = make_plot(source, city)
     city_select.on_change('value', update_plot)
 	
-    # Saving the plot into disk
+    # add to document
     output_file("./Plots/Op5.html", title="Optional Task # 5")
     save(HBox(city_select, plot))
 	
-	#curdoc().add_root(HBox(city_select, plot))
+#    curdoc().add_root(HBox(city_select, plot))
 
 if __name__ == '__main__':
     Main()
 
-#Main()
