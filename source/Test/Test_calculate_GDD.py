151eb9332006aac9bb85fb4656acaafaf1186302
Mohammad Hassan <mahmud.sust@gmail.com>
2016-06-15 01:23:11 -0230

    Update Test_calculate_GDD.py

--- source/Test/Test_calculate_GDD.py
+++ source/Test/Test_calculate_GDD.py
@@ -20,7 +20,7 @@ def calculate_GDD(Data, baseTemp):
 	
 def Main():
     baseTemp = 10
-    
+    # Observed Min, Max values for test purpose. 
     obs = {'Max' : pd.Series([13.1, 4.9, 13.4, 20.7, 27.9, 18.4, 15.8, 17.9, 11.8, 15.1, 21.5, 19.8, 10.6, 15.7, 14.9]),
            'Min' : pd.Series([0.7, -1.6, -5.6, -0.6, 3.1, 3.4, -1.2, 2.4, 0.5, -1.4, 0.1, 3, 0.6, -3.2, 1])}
     obs_df = pd.DataFrame(obs)
@@ -31,11 +31,11 @@ def Main():
     obs_GDD = list(obs_GDD)
     obs_GDD = [ round(elem, 2) for elem in obs_GDD ]
     
+    # Sample expected values including manually calculated GDD values to compare with Observed values. 
     exp = {'Max' : pd.Series([13.1, 4.9, 13.4, 20.7, 27.9, 18.4, 15.8, 17.9, 11.8, 15.1, 21.5, 19.8, 10.6, 15.7, 14.9]),
-           'Min' : pd.Series([0.7, -1.6, -5.6, -0.6, 3.1, 3.4, -1.2, 2.4, 0.5, -1.4, 0.1, 3, 0.6, -3.2, 1])}
+           'Min' : pd.Series([0.7, -1.6, -5.6, -0.6, 3.1, 3.4, -1.2, 2.4, 0.5, -1.4, 0.1, 3, 0.6, -3.2, 1]), 
+           'GDD' : pd.Series([0, 0, 0, 0.05, 5.55, 6.45, 6.45, 6.60, 6.60, 6.60, 7.40, 8.80, 8.80, 8.80, 8.80])}
     exp_df = pd.DataFrame(exp)
-    
-    exp_df['GDD'] = pd.Series([0, 0, 0, 0.05, 5.55, 6.45, 6.45, 6.60, 6.60, 6.60, 7.40, 8.80, 8.80, 8.80, 8.80])
     
     exp_GDD = np.array(exp_df['GDD'])
     exp_GDD = list(exp_GDD)
